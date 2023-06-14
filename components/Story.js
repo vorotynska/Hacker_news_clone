2353679527ef1c1f0d92d6e0c39b9e790f4fd24a
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-06-14 23:16:17 +0200

    commit

--- components/Story.js
+++ components/Story.js
@@ -1,9 +1,8 @@
 export default function Story(story) {
-    console.log(story);
     return `
       <div class="story">
         <div> 
-          <span class="gray">${story.index}</span>
+          <span class="gray">${story.index || ""}</span>
           <span class="upvote">▲</span>
           <a href="${story.url}">${story.title}</a>
           <span>(${story.domain})</span>
@@ -16,9 +15,9 @@ export default function Story(story) {
               ${story.comments_count} comments
             </a>
             |
-            <span class="favorite">
+            <span class="favorite" data-story='${JSON.stringify(story)}'>
               <img class="heart" src="https://icon.now.sh/heart/ccc">
-              Add To Favorites
+              ${story.isFavorite ? "Remove From Favorites" : "Add To Favorites"}
             </span>
           </div>
         </div>
--- pages/stories.js
+++ pages/stories.js
@@ -1,13 +1,33 @@
-import Story from '../components/Story.js'
+import Story from '../components/Story.js';
 import view from '../utils/view.js';
+import baseUrl from '../utils/baseUrll.js';
+import checkFavorite from '../utils/checkFavorite.js';
+import store from '../store.js';
 
 export default async function Stories(path) {
+    const {
+        favorites
+    } = store.getState();
     const stories = await getStories(path);
     const hasStories = stories.length > 0;
 
     view.innerHTML = `<div>
-    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'}
+    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1, isFavorite: checkFavorite(favorites, story) })).join('') : 'No stories'}
   </div>`;
+
+    document.querySelectorAll('.favorite').forEach(favoriteButton => {
+        favoriteButton.addEventListener('click', async function () {
+            const story = JSON.parse(this.dataset.story);
+            const isFavorited = checkFavorite(favorites, story);
+            store.dispatch({
+                type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE",
+                payload: {
+                    favorite: story
+                }
+            })
+            await Stories(path);
+        });
+    });
 }
 
 async function getStories(path) {
@@ -18,7 +38,14 @@ async function getStories(path) {
     } else if (isNewRoute) {
         path = '/newest';
     }
-    const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+    const response = await fetch(`${baseUrl}${path}`);
     const stories = await response.json();
     return stories;
 }
+
+// https://node-hnapi.herokuapp.com
+
+// / (Top) -> /news
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show 
